d483bdaef564cb7421587772b4b3b1db8a48bc28
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-02-05 20:50:53 +0100

    Done 912.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/arraysandhashing/sortanarray_912/Main.java
@@ -0,0 +1,12 @@
+package com.bliznichenka.arraysandhashing.sortanarray_912;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(Arrays.toString(solution.sortArray(nums)));
+    }
+}
