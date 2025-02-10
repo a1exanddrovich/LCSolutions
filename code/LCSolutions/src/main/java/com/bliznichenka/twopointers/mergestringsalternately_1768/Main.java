70a7c4b94a9fce12970cb9e3a51cf620c6f88af4
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-02-10 20:11:49 +0100

    Done 1768.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/twopointers/mergestringsalternately_1768/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.twopointers.mergestringsalternately_1768;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String word1 = "abc";
+        String word2 = "pqr";
+        System.out.println(solution.mergeAlternately(word1, word2));
+    }
+}
