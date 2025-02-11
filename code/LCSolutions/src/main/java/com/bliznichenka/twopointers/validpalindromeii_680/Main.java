0f4fb4f52713046a22cd376ec595d996aebb8067
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-02-11 19:56:52 +0100

    Done 680.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/twopointers/validpalindromeii_680/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.twopointers.validpalindromeii_680;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "abca";
+        System.out.println(solution.validPalindrome(s));
+    }
+}
