0815a961325200af95b26a4a7bdaf9f46080be7f
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-02-17 20:05:22 +0100

    Done 424.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/slidingwindow/longestrepeatingcharacterreplacement_424/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.slidingwindow.longestrepeatingcharacterreplacement_424;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "ABAB";
+        int k = 2;
+        System.out.println(solution.characterReplacement(s, k));
+    }
+}
