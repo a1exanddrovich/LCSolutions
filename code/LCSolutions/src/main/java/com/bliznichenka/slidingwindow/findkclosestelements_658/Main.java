f1029ebb1dcc626e986f491afc7dcc1e633a81aa
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-02-18 22:48:29 +0100

    Done 658.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/slidingwindow/findkclosestelements_658/Main.java
@@ -0,0 +1,12 @@
+package com.bliznichenka.slidingwindow.findkclosestelements_658;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        int x = 3;
+        System.out.println(solution.findClosestElements(arr, k, x));
+    }
+}
