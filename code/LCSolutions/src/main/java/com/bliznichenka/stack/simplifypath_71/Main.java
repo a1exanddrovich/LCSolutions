86d268025fb91f2dd494a3168066e128f519861c
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-02-24 21:25:40 +0100

    Done 71.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/stack/simplifypath_71/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.stack.simplifypath_71;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String path = "/.../a/../b/c/../d/./";
+        System.out.println(solution.simplifyPath(path));
+    }
+}
