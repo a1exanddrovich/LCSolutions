1f349306bf8464cb7738a659add4b36ce66e14d3
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-02 16:45:35 +0100

    Done 81.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/binarysearch/searchinrotatedsortedarrayii_81/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.binarysearch.searchinrotatedsortedarrayii_81;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] arr = {11, 0, 1, 1, 1, 1, 1, 1, 7, 8, 9, 10};
+        int target = 1;
+        System.out.println(s.search(arr, target));
+    }
+}
