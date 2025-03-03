f68c84ac6c36cc34dd1e21e2d2791f37ba8aa2f7
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-03 22:15:03 +0100

    Done 410.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/binarysearch/splitarraylargestsum_410/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.binarysearch.splitarraylargestsum_410;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {7, 2, 5, 10, 8};
+        int k = 2;
+        System.out.println(solution.splitArray(nums, k));
+    }
+}
