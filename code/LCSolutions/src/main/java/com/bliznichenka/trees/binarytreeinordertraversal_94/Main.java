a4190ad8481cf8e52cd9d43c98ca34444e4d506c
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-11 20:47:35 +0100

    Added iterative solution to the 94 (Modifications Only).

--- code/LCSolutions/src/main/java/com/bliznichenka/trees/binarytreeinordertraversal_94/Main.java
+++ code/LCSolutions/src/main/java/com/bliznichenka/trees/binarytreeinordertraversal_94/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        TreeNode root = TreeNodeUtils.getDefault();
+        TreeNode root = TreeNodeUtils.deserializePreorder("1 2 3 X X 4 X X 5 6 X X X");
 
         System.out.println(s.inorderTraversal(root));
     }
