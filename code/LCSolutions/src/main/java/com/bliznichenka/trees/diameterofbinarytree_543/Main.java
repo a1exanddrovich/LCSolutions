7e650864f1c0c37637df18f7d59d44f8673e0436
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-12 21:44:28 +0100

    Done 543.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/trees/diameterofbinarytree_543/Main.java
@@ -0,0 +1,13 @@
+package com.bliznichenka.trees.diameterofbinarytree_543;
+
+import com.bliznichenka.trees.TreeNode;
+import com.bliznichenka.trees.TreeNodeUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = TreeNodeUtils.getDefault();
+
+        System.out.println(s.diameterOfBinaryTree(root));
+    }
+}
