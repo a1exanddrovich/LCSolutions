b91e7199fb45c55d304d41985ff21d0f025e6598
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-22 14:12:47 +0100

    Done 77.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/backtracking/combinations_77/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.backtracking.combinations_77;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int n = 4;
+        int k = 2;
+        System.out.println(s.combine(n, k));
+    }
+}
