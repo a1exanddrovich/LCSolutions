a271bd60abbf5af0f42ec698eeab55a1cb84cfe7
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-23 18:47:35 +0100

    Done 52.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/backtracking/nqueensii_52/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.backtracking.nqueensii_52;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int n = 4;
+        System.out.println(s.totalNQueens(n));
+    }
+}
