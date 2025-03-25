d57792b7afd870781621c313901d2c66e48d4f2e
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-25 18:05:14 +0100

    added bfs to the 200 (Modifications Only).

--- code/LCSolutions/src/main/java/com/bliznichenka/graphs/numberofislands_200/Solution.java
+++ code/LCSolutions/src/main/java/com/bliznichenka/graphs/numberofislands_200/Solution.java
@@ -2,9 +2,7 @@ package com.bliznichenka.graphs.numberofislands_200;
 
 import javafx.util.Pair;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     public int numIslands(char[][] grid) {
@@ -45,6 +43,43 @@ public class Solution {
         );
         for (List<Integer> dir : dirs) {
             dfs(grid, i + dir.get(0), j + dir.get(1), visited);
+        }
+    }
+
+    private static void bfs(
+            char[][] grid,
+            int i,
+            int j,
+            Set<Pair<Integer, Integer>> visited
+    ) {
+        Deque<Pair<Integer, Integer>> deque = new ArrayDeque<>();
+
+        Pair<Integer, Integer> pair = new Pair<>(i, j);
+        visited.add(pair);
+        deque.addFirst(pair);
+        while (!deque.isEmpty()) {
+            Pair<Integer, Integer> polled = deque.pollLast();
+            int row = polled.getKey();
+            int col = polled.getValue();
+
+            List<List<Integer>> dirs = List.of(
+                    List.of(0, -1),
+                    List.of(1, 0),
+                    List.of(0, 1),
+                    List.of(-1, 0)
+            );
+            for (List<Integer> dir : dirs) {
+                int nextI = row + dir.get(0);
+                int nextJ = col + dir.get(1);
+                if (nextI >= 0 && nextI < grid.length
+                        && nextJ >= 0 && nextJ < grid[0].length
+                        && !visited.contains(new Pair<>(nextI, nextJ))
+                        && grid[nextI][nextJ] == '1') {
+                    Pair<Integer, Integer> nextPair = new Pair<>(nextI, nextJ);
+                    visited.add(nextPair);
+                    deque.addFirst(nextPair);
+                }
+            }
         }
     }
 }
