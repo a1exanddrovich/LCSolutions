6e69b790cd565de05f8a700f60385d79bada0548
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-28 21:16:06 +0100

    Done 127.

--- code/LCSolutions/src/main/java/com/bliznichenka/graphs/minimumheighttrees_310/Main.java
+++ code/LCSolutions/src/main/java/com/bliznichenka/graphs/minimumheighttrees_310/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        int n = 4;
-        int[][] edges = {{1, 0}, {1, 2}, {1, 3}};
+        int n = 9;
+        int[][] edges = {{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4}, {4, 8}, {5, 6}, {5, 7}};
         System.out.println(s.findMinHeightTrees(n, edges));
     }
 }
