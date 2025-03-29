bc821c1a37b5053be661b0eb144beaef8ff406fa
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-03-29 19:25:04 +0100

    Done 1046.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/heappriorityqueue/laststoneweight_1046/Main.java
@@ -0,0 +1,12 @@
+package com.bliznichenka.heappriorityqueue.laststoneweight_1046;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] stones = {2, 7, 4, 1, 8, 1};
+        System.out.println(s.lastStoneWeight(stones));
+    }
+
+}
