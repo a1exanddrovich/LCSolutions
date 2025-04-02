7945ce0edcee431cf65c6f19fc88760b9ebc221d
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-02 13:37:12 +0200

    Done 213.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/dynamicprogramming1d/houserobberii_213/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.dynamicprogramming1d.houserobberii_213;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {10, 9, 2, 1, 3, 10, 5, 9};
+        System.out.println(s.rob(nums));
+    }
+}
