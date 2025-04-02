2d040998391304fca095b08778b09b6e6d0583c4
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-02 14:08:36 +0200

    Done 5.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/dynamicprogramming1d/longestpalindromicsubstring_5/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.dynamicprogramming1d.longestpalindromicsubstring_5;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String str = "babad";
+        System.out.println(s.longestPalindrome(str));
+    }
+}
