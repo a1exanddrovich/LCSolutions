11b0a6f17dc461f5f68266818a5dfbc5be9d641e
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-03 17:53:48 +0200

    Done 377.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/dynamicprogramming1d/combinationsumiv_377/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.dynamicprogramming1d.combinationsumiv_377;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {1,2,3};
+        int target = 4;
+        System.out.println(s.combinationSum4(nums, target));
+    }
+}
