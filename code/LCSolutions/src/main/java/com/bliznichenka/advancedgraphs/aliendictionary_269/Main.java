1cb7b3ddea820c71bffc76d4ed6c6c8ebb185de5
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-05 19:32:58 +0200

    Done 269.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/advancedgraphs/aliendictionary_269/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.advancedgraphs.aliendictionary_269;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String[] words = {"wrt","wrf","er","ett","rftt"};
+        System.out.println(s.alienOrder(words));
+    }
+}
