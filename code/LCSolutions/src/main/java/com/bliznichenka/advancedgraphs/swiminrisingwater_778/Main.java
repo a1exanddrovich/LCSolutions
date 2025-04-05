51874e69d0c48856bfc5e3149eba10dc0a90a87f
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-05 18:13:56 +0200

    Done 778.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/advancedgraphs/swiminrisingwater_778/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.advancedgraphs.swiminrisingwater_778;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[][] grid = {{0, 2}, {1, 3}};
+        System.out.println(s.swimInWater(grid));
+    }
+}
