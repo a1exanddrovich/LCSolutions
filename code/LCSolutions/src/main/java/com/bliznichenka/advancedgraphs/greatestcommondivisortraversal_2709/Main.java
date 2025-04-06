e3bcf92c156e578bd636141623ed14cd3b0e0284
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-06 17:35:19 +0200

    Done 2709.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/advancedgraphs/greatestcommondivisortraversal_2709/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.advancedgraphs.greatestcommondivisortraversal_2709;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {2, 3, 6};
+        System.out.println(s.canTraverseAllPairs(nums));
+    }
+}
