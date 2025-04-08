fd77fafe591307675f5d1fb3281d282870f10c49
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-08 19:57:42 +0200

    Done 45.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/greedy/jumpgameii_45/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.greedy.jumpgameii_45;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(solution.jump(nums));
+    }
+}
