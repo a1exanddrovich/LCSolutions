6cf5d8610822f7c4d92f24c2c6bb46c0f41a8001
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-10 11:31:39 +0200

    Done 62.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/dynamicprogramming2d/uniquepaths_62/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.dynamicprogramming2d.uniquepaths_62;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int m = 3, n = 7;
+        System.out.println(s.uniquePaths(m, n));
+    }
+}
