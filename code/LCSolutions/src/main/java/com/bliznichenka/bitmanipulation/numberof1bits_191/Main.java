7a1ecee4925b85d5ea73d300168918bf7400462a
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-15 22:07:47 +0200

    Done 191.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/bitmanipulation/numberof1bits_191/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.bitmanipulation.numberof1bits_191;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 23;
+        System.out.println(solution.hammingWeight(n));
+    }
+}
