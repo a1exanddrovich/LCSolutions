c1cd2ae0888cc07e7d4246a5e4bcbcff95bc7878
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-15 16:28:04 +0200

    Done 50.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/mathandgeometry/powxn_50/Main.java
@@ -0,0 +1,9 @@
+package com.bliznichenka.mathandgeometry.powxn_50;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.myPow(2, 2));
+    }
+}
