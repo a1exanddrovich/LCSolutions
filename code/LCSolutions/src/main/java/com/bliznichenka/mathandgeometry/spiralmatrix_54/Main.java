8cbd0bdda384590d1f8a7ac426fb64a0e2a82eb8
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-15 11:59:43 +0200

    Done 54.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/mathandgeometry/spiralmatrix_54/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.mathandgeometry.spiralmatrix_54;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] matrix = {
+                {1, 4, 5}};
+        System.out.println(solution.spiralOrder(matrix));
+    }
+}
