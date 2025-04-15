afcf1a4c68e3bdb5e1a24474293aa2699e4982e0
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-15 22:57:41 +0200

    Done 338.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/bitmanipulation/countingbits_338/Main.java
@@ -0,0 +1,12 @@
+package com.bliznichenka.bitmanipulation.countingbits_338;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 2;
+        System.out.println(Arrays.toString(solution.countBits(n)));
+    }
+}
