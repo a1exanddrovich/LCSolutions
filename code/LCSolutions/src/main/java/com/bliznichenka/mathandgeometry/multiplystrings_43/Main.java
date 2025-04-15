0431b43c2a3825708dc196f3f868f91d5d796039
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-15 18:07:46 +0200

    Done 43.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/mathandgeometry/multiplystrings_43/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.mathandgeometry.multiplystrings_43;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String num1 = "123";
+        String num2 = "456";
+        System.out.println(solution.multiply(num1, num2));
+    }
+}
