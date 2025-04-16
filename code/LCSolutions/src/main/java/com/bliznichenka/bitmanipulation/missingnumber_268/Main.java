a90f2240e84b946641f40ba17aacc20949a5f5b7
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-16 12:44:43 +0200

    Done 268.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/bitmanipulation/missingnumber_268/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.bitmanipulation.missingnumber_268;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {0, 1};
+        System.out.println(solution.missingNumber(nums));
+    }
+}
