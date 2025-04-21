085937da31cca354fcaa2b74a221e00bfda2b535
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-04-21 20:58:46 +0200

    Done 724.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/arraysandhashing/findpivotindex_724/Main.java
@@ -0,0 +1,10 @@
+package com.bliznichenka.arraysandhashing.findpivotindex_724;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1, 7, 3, 6, 5, 6};
+        System.out.println(solution.pivotIndex(nums));
+    }
+}
