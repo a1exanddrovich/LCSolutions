1605e6b578bb0a6a9d497ae9de3c6fc83e6b3a7b
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-05-03 18:54:02 +0200

    Done 605.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/arraysandhashing/canplaceflowers_605/Main.java
@@ -0,0 +1,11 @@
+package com.bliznichenka.arraysandhashing.canplaceflowers_605;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] flowerbed = {1, 1, 0, 0};
+        int n = 1;
+        System.out.println(solution.canPlaceFlowers(flowerbed, n));
+    }
+}
