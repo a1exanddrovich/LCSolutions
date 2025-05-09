fcb560a3866cd2a1cd0e96f4af8079ae9234e375
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-05-09 20:28:45 +0200

    Done 946.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/stack/validatestacksequences_946/Main.java
@@ -0,0 +1,12 @@
+package com.bliznichenka.stack.validatestacksequences_946;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {4, 5, 3, 2, 1};
+        System.out.println(s.validateStackSequences(arr1, arr2));
+    }
+
+}
