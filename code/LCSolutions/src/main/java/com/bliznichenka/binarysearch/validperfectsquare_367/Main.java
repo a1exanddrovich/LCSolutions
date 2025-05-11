c7082b80d6e69bfa1a4d8a402720b48182ae0d2f
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-05-11 20:16:58 +0200

    Done 367.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/binarysearch/validperfectsquare_367/Main.java
@@ -0,0 +1,8 @@
+package com.bliznichenka.binarysearch.validperfectsquare_367;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isPerfectSquare(2147483647));
+    }
+}
