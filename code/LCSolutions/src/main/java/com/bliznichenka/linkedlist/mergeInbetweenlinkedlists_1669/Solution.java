5a4563e0969200b8dbf54c135b495354f49b4bbd
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-05-15 18:17:24 +0200

    Done 1669.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/linkedlist/mergeInbetweenlinkedlists_1669/Solution.java
@@ -0,0 +1,32 @@
+package com.bliznichenka.linkedlist.mergeInbetweenlinkedlists_1669;
+
+import com.bliznichenka.linkedlist.ListNode;
+
+public class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode i = list1;
+        int index = 0;
+        while (index != a - 1) {
+            i = i.next;
+            index++;
+        }
+
+        ListNode nextI = i.next;
+        i.next = list2;
+        i = nextI;
+        index++;
+
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+
+        while (index != b + 1) {
+            i = i.next;
+            index++;
+        }
+
+        list2.next = i;
+
+        return list1;
+    }
+}
