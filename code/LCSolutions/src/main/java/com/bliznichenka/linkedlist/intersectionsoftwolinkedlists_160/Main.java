4aad36cb855d90a187a7700a0b664f5a7a14c4d8
Aliaksei Bliznichenka <aliaksei_bliznichenka@epam.com>
2025-05-15 18:04:32 +0200

    Done 160.

---
+++ code/LCSolutions/src/main/java/com/bliznichenka/linkedlist/intersectionsoftwolinkedlists_160/Main.java
@@ -0,0 +1,20 @@
+package com.bliznichenka.linkedlist.intersectionsoftwolinkedlists_160;
+
+import com.bliznichenka.linkedlist.ListNode;
+import com.bliznichenka.linkedlist.ListNodeUtils;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode headA = ListNodeUtils.fromList(List.of(4, 1));
+        ListNode headB = ListNodeUtils.fromList(List.of(5, 6, 1));
+        ListNode headC = ListNodeUtils.fromList(List.of(8, 4, 5));
+        ListNodeUtils.intersectAt(headA, headB, headC);
+
+        System.out.println(ListNodeUtils.toList(s.getIntersectionNode(headA, headB)));
+    }
+
+}
